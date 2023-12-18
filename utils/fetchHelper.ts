a8336bdc61eebebfd3d96223333d0333d36effa6
rajeshraman63 <rajeshraman63@gmail.com>
2023-12-19 00:43:00 +0530

    updated fetchhelper.ts file

--- utils/fetchHelper.ts
+++ utils/fetchHelper.ts
@@ -1,25 +1,36 @@
 interface IFetchProps {
   url: string;
   methodType?: string;
-  headers?: any;
+  headers?: Record<string, string>;
   body?: any;
 }
+
 export const customFetch = async (props: IFetchProps) => {
   const storedToken = localStorage.getItem("token");
-  // const storedToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGVtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE3MDI5MTI4MDUsImV4cCI6MTcwMjkxNjQwNX0.McrtjdK2aAjSC5vj8zcipa3yvNKXYt99wimVtE";
 
   try {
     const response = await fetch(props.url, {
       method: props.methodType || "GET",
       headers: {
         "Content-Type": "application/json",
-        authorization: storedToken as string,
+        Authorization: storedToken ? `Bearer ${storedToken}` : "",
         ...props.headers,
       },
+      body: props.body ? JSON.stringify(props.body) : undefined,
     });
-    //   const data = await response.json();
-    return response.json();
-  } catch (e) {
-    console.log("error while making api call ");
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const contentType = response.headers.get("content-type");
+    if (contentType && contentType.includes("application/json")) {
+      return response.json();
+    } else {
+      return response.text(); // or handle other content types
+    }
+  } catch (error) {
+    console.error("Error while making API call:", error);
+    return { error: "Something went wrong" };
   }
 };
